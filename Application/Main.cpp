deb9d018cfdbb425ef45ca6e51653afb9cd45660
Birdy Bird <edixon@student.neumont.edu>
2022-10-06 15:39:50 -0600

    Updated Renderer GL Context to Core.

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -10,46 +10,17 @@ int main(int argc, char** argv)
 
 	en::__renderer.newWindow("Application", 800, 600);
 
-	float angle = 0.0f;
-	float val = 0.001f;
-	en::Vector2 pos {0, 0};
-
 	bool quit = false;
 	while (!quit)
 	{
 		en::Engine::Instance().Update();
 
 		if (en::__inputsys.getKeyState(en::key_escape) == en::InputSystem::KeyState::PRESSED) quit = true;
-		if (en::__inputsys.getKeyState(en::key_left) == en::InputSystem::KeyState::HELD) pos.x -= en::__time.ci_time * 2;
-		if (en::__inputsys.getKeyState(en::key_right) == en::InputSystem::KeyState::HELD) pos.x += en::__time.ci_time * 2;
 
-		float rrr = en::randomf(0.0, 1.0);
-		float ggg = en::randomf(0.0, 1.0);
-		float bbb = en::randomf(0.0, 1.0);
 		en::__renderer.beginFrame({ 0.0f, 0.0f, 0.0f, 1.0f });
 
-		angle += 180.0f * en::__time.ci_time;
-
-		glPushMatrix();
-
-		// val += 0.001f;
-		glScalef(0.999 - val, 0.999 - val, 0.999 - val);
-		glRotatef(angle, 1, 0, 0);
-		glTranslatef(pos.x, pos.y, 0);
-
-		glBegin(GL_TRIANGLES);
-
-		glColor3f(0.1f, 0.0f, 0.0f);
-		glVertex2f(0.8f, 0.8f);
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(0.0f, -0.8f);
-		glColor3f(0.0f, 0.0f, 0.1f);
-		glVertex2f(-0.8f, 0.8f);
-
-		glEnd();
-
-		glPopMatrix();
-
+		// DRAW
+		
 		en::__renderer.endFrame();
 	}
 
